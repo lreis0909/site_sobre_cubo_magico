0a70bc1ec351b36527436ef64eac4d1c1ccc497e
Letícia Reis <40440598+lreis0909@users.noreply.github.com>
2020-05-22 13:54:13 -0300

    arquivos adicionados via upload

---
+++ custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+        $('.carousel').carousel({
+               numVisible: 7
+        });
+        $('.carousel.carousel-slider').carousel({
+                fullWidth: true
+        });
+        $('.next').click(function(){
+                $('.carousel').carousel('next');
+        });
+        $('.prev').click(function(){
+                $('.carousel').carousel('prev');
+        });
+});
+
